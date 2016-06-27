4b0460a16761b52841d0a0705f373ce2117fa7ba
Cess <cessmbuguar@gmail.com>
2016-06-27 11:24:57 +0300

    calculate discounts

--- chapter5/array.rb
+++ chapter5/array.rb
@@ -2,11 +2,21 @@ def total(prices)
   index = 0
  amount = 0
 while index < prices.length
-amount += prices[index]
+amount -= prices[index]
 index +=1
 end
 amount
 end
 prices = [90.6, 78.34, 78,23,45,234]
-puts format("%.2f", total(prices))
+def discounts(prices)
+  index = 0
+  while index < prices.length
+    amount_off = prices[index] / 3.0
+    puts format("Your discount is: $%.2f", amount_off)
 
+  index+=1
+  end
+end
+discounts(prices)
+
+puts format("%.2f", total(prices))
