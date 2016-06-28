c58a519c7123e3a47b03ebfb816a42ff426e01cc
Cess <cessmbuguar@gmail.com>
2016-06-28 12:09:17 +0300

    resolve null issues

--- chapter7/votes.rb
+++ chapter7/votes.rb
@@ -1,16 +1,16 @@
 lines = []
 File.open("votes.txt") do |file|
-lines = file.readlines
+  lines = file.readlines
 end
 votes = Hash.new(0)
 lines.each do |line|
-name = line.chomp
-name.upcase!
-if votes[name] 
-  votes[name] += 1
-else
-  votes[name] = 1
-end
+  name = line.chomp
+  name.upcase!
+  if votes[name] 
+    votes[name] += 1
+  else
+    votes[name] = 1
+  end
 end
 p votes
 
