f00246aabe8064d1db4f06eb1b9871f5b124d585
Cess <cessmbuguar@gmail.com>
2016-06-29 09:23:34 +0300

    counter increment

---
+++ chapter8/counter.rb
@@ -0,0 +1,17 @@
+class   Counter
+  def intialize
+    @count = 0
+  end
+  def increment
+    @count += 1
+    puts @count
+  end
+end
+  a = Counter.new
+  b = Counter.new
+  b = c
+  d=c
+  a.increment
+  b.increment
+  c.increment
+  d.increment
