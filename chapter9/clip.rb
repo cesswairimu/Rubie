ebadaf825407d683d8e9f96532c8fbef788930fb
Cess <cessmbuguar@gmail.com>
2016-06-29 12:53:53 +0300

    using a module

--- chapter9/clip.rb
+++ chapter9/clip.rb
@@ -1,21 +1,32 @@
-class Clip
-attr_accessor :comments
-def initialize
-  @comments = []
-end
+module AcceptComments
+  def comments
+    if @comments
+      @comments
+    else
+      @comments = []
+    end
+  end
 def add_comment(comment)
   comments << comment
 end
-def play
+end
+
+class Clip
+  def play
   puts "Playing #{object_id}"
 end
 end
+
 class Video < Clip
+ include AcceptComments
   attr_accessor :resolution
 end
+
 class Song < Clip
+ include AcceptComments
   attr_accessor :beats_per_minute
 end
+
 video = Video.new
 video.add_comment("This music is dope")
 video.add_comment("Wierd ending")
