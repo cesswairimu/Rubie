3973ad02792d1876382aaf12a230f00c7718ca79
Cess <cessmbuguar@gmail.com>
2016-07-01 10:50:09 +0300

    handling an exception

---
+++ chapter12/oven.rb
@@ -0,0 +1,32 @@
+class Oven
+  attr_accessor :contents
+  def power_onn
+    puts "Turning oven is on"
+    @state = "on"
+  end
+  def power_off
+    puts "Turnind oven is off"
+    @state = "off"
+  end
+
+  def bake
+      unless @state == "on"
+        return "You need to put the oven on first"
+      end
+        if @contents == nil
+          puts "There are no contents in the oven"
+        end
+   "goldenbrown  #{ contents }"
+
+  end
+end
+dinner = ['pie', 'chicken', 'rice']
+oven = Oven.new
+oven.power_onn
+dinner.each do |item|
+ oven.contents = item
+ puts "Serving #{oven.bake} "
+end
+
+  
+  
