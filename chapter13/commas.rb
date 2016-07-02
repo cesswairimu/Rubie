4d0e26382b50ea2137f7c75f1de2216f9593db82
Cess <cessmbuguar@gmail.com>
2016-07-02 09:58:37 +0300

    use of slice method

---
+++ chapter13/commas.rb
@@ -0,0 +1,14 @@
+class ListCommas
+  attr_accessor :items
+  def join
+    last_item = "and #{items.last}"
+    other_items = items.slice(0,items.length - 1).join( ', ')
+    "#{other_items} #{last_item}"
+  end
+end
+two_subjects = ListCommas.new
+two_subjects.items = ['my parents are', 'so adorable']
+puts "A photo of #{two_subjects.join}"
+three_subjects = ListCommas.new
+three_subjects.items = ['wierdos are so funny', 'but caring sometimes', 'but they are just crazy']
+puts "A photo of the #{three_subjects.join}"
