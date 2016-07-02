83eb7af8c4bb7f279d1bf1624ec3c8359b05e34f
Cess <cessmbuguar@gmail.com>
2016-07-02 14:47:15 +0300

    testing falsy and truthy

---
+++ chapter13/test_something.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+
+class TestSomething  < MiniTest::Test
+  
+ def test_true_assertion
+    assert(true)
+  end
+
+  def test_false_assertion 
+  assert(false)
+  end
+end
