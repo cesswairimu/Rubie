d593cf2c26a481eedd689f5f48364c97320ce5c2
Cess <cessmbuguar@gmail.com>
2016-12-29 11:46:33 +0300

    change root to visitors new template

--- learn-rails/config/routes.rb
+++ learn-rails/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'visitors/new'
+  root to: 'visitors#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: redirect('about.html')
 end
