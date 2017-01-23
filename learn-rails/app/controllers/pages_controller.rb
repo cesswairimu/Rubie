65b58e72c9a65b4a9b0df94d7e1634fbd21503ba
Cess <cessmbuguar@gmail.com>
2017-01-23 23:35:16 +0300

    invalid exceptions

--- learn-rails/app/controllers/pages_controller.rb
+++ learn-rails/app/controllers/pages_controller.rb
@@ -6,4 +6,17 @@ class PagesController < ApplicationController
     params = { name: "cess", email:"cess@her.com", content: "Lorem ipsum the hdsrtdtrd"}
 
   end
+  def process_form
+    if params[:name].blank?
+      raise 'Name is blank!'
+    end
+    if params[:email].blank?
+      raise 'Email is blank!'
+    end
+    if params[:content].blank?
+      raise 'Message is blank!'
+    end
+    message = "Received request from #{params[:contact][:name]}"
+    redirect_to root_path, :notice => message
+  end
 end
