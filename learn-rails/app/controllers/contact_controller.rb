c9631d68c15fc4c65b8b9868daac7064a5366896
Cess <cessmbuguar@gmail.com>
2017-01-24 23:44:08 +0300

    remove validations from pages to contact controllers

--- learn-rails/app/controllers/contact_controller.rb
+++ learn-rails/app/controllers/contact_controller.rb
@@ -1,2 +1,16 @@
 class ContactController < ApplicationController
+   def process_form
+    if params[:name].blank?
+      raise 'Name is blank!'
+    end
+    if params[:email].blank?
+      raise 'Email is blank!'
+    end
+    if params[:content].blank?
+      raise 'Message is blank!'
+    end
+    message = "Received request from #{params[:contact][:name]}"
+    redirect_to root_path, :notice => message
+  end
+
 end
