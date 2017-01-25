80dc18cc608bdfde0d47d15db2d367df13a69224
Cess <cessmbuguar@gmail.com>
2017-01-25 23:50:47 +0300

    validating from the model

--- learn-rails/app/models/contact.rb
+++ learn-rails/app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  REGEX = /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates :name, presence: true
+  validates :email, presence:true, format: { with: REGEX }
+  validates :content, presence:true
 end
