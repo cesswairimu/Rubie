df9f84f0527debfac60c8abf49c817b86c9028b8
Cess <cessmbuguar@gmail.com>
2017-01-27 11:15:07 +0300

    add contact resources :smiley:

--- learn-rails/config/routes.rb
+++ learn-rails/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
 
-  get 'contacts/new'
 
   root to: 'visitors#new'
   get 'about' => 'pages#about'
   get 'contact' => 'contacts#new'
+  resources :contacts
 
 end
