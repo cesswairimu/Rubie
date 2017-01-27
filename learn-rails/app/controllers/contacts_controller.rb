992eeee26d554d348bcc0998aa12455840a4c75e
Cess <cessmbuguar@gmail.com>
2017-01-27 10:58:10 +0300

    new contact object and exception errors

--- learn-rails/app/controllers/contacts_controller.rb
+++ learn-rails/app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+  def process_form
+    if params[:name].blank?
+      raise 'Name is blank!'
+    end
+    if params[:email].blank?
+      raise 'Email is blank!'
+    end
+    if params[:content].blank?
+      raise 'Message is blank!'
+    end
+    message = "Received request from #{params[:contact][:name]}"
+    redirect_to root_path, :notice => message
+  end
+
 end
