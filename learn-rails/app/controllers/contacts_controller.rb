ea63838aca4a422ef9833d766b0edb94712dd245
Cess <cessmbuguar@gmail.com>
2017-01-28 11:54:05 +0300

    create action and permitting form params

--- learn-rails/app/controllers/contacts_controller.rb
+++ learn-rails/app/controllers/contacts_controller.rb
@@ -2,6 +2,16 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      flash[:notice]="Message from #{@contact.name}"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
   def process_form
     if params[:name].blank?
       raise 'Name is blank!'
@@ -16,4 +26,8 @@ class ContactsController < ApplicationController
     redirect_to root_path, :notice => message
   end
 
+  private
+  def secure_params
+    params.require(:contact).permit(:name,:email,:content)
+  end
 end
